851fdc22cdc13b5fb9955165cc01c12d23eba48f
volodymyr.shpynta <volodymyr.shpynta@arrise.com>
2024-08-21 14:30:08 +0200

    Adjust custom searchable dropdown - add functionality to select option by pressing 'enter' button.

--- src/main/webapp/js/searchable-dropdown.js
+++ src/main/webapp/js/searchable-dropdown.js
@@ -52,12 +52,12 @@
 
 class SearchableDropdown {
 
+    // Private constants and variables:
     #DROPDOWN_ACTIVE_CLASS = "active";
-    #OPTION_SELECTED_CLASS = "selected";
+    #OPTION_HIGLIGHTED_CLASS = "selected";
     #CUSTOM_VALUE_ATTR_NAME = 'customvalue';
 
     #dropdownSelector;
-
     #dropdownWrapper;
     #hiddenSourceSelect;
     #selectButton;
@@ -70,7 +70,7 @@ class SearchableDropdown {
         this.#initSearchableDropdown();
     }
 
-    // -------------- Public API: ----------------
+    // Public methods:
 
     getSelectedValue() {
         return this.#hiddenSourceSelect.value;
@@ -93,12 +93,12 @@ class SearchableDropdown {
     updateDropdown(items) {
         this.clearSelectedValue();
 
-        const options = this.#createOptions(items)
+        const options = this.#createOptions(items);
         this.#updateHiddenSelectOptions(options);
         this.#populateSelectOptions(options, options);
     }
 
-    // -------------- Private API: ----------------
+    // Private methods:
 
     #initSearchableDropdown() {
         this.#initDropdownWrapper();
@@ -109,33 +109,36 @@ class SearchableDropdown {
         const allOptions = Array.from(this.#hiddenSourceSelect.options);
         this.#populateSelectOptions(allOptions, allOptions);
 
-        this.#searchInput.addEventListener("keyup", keyUpEvent => {
-            if (keyUpEvent.key === "ArrowDown") {
+        this.#searchInput.addEventListener("keydown", keyDownEvent => {
+            if (keyDownEvent.key === "ArrowDown") {
+                keyDownEvent.preventDefault(); // Prevent the cursor from moving in the input field
                 this.#highlightNextOption();
-            } else if (keyUpEvent.key === "ArrowUp") {
+            } else if (keyDownEvent.key === "ArrowUp") {
+                keyDownEvent.preventDefault(); // Prevent the cursor from moving in the input field
                 this.#highlightPrevOption();
-            } else { //handle filtering based on search input value
-                this.#filterOptionsAccordingToSearchInput();
+            } else if (keyDownEvent.key === "Enter") {
+                keyDownEvent.preventDefault(); // Prevent form submission
+                this.#selectHighlightedOption();
             }
         });
 
-        //TODO: implement:
-        this.#searchInput.addEventListener("submit", () => {
-            console.log("Key down on search input pressed")
-            return false;
+        // listening to 'input' event for filtering:
+        this.#searchInput.addEventListener("input", () => {
+            this.#filterOptionsAccordingToSearchInput();
         });
 
         this.#selectButton.addEventListener("click", () => {
             if (this.#getAttribute(this.#dropdownWrapper, "aria-disabled").toLowerCase() !== "true") {
                 this.#dropdownWrapper.classList.toggle(this.#DROPDOWN_ACTIVE_CLASS);
-                //set focus on search input in case of open dropdown:
+                // set focus on search input and scroll to the selected option in case of open dropdown:
                 if (this.#dropdownWrapper.classList.contains(this.#DROPDOWN_ACTIVE_CLASS)) {
                     this.#searchInput.focus();
+                    this.#scrollToHighlightedOption();
                 }
             }
         });
 
-        //close the dropdown on click mouse outside the dropdown:
+        // close the dropdown on click mouse outside the dropdown:
         document.addEventListener("click", mouseEvent => {
             if (!this.#dropdownWrapper.contains(mouseEvent.target)) {
                 this.#dropdownWrapper.classList.remove(this.#DROPDOWN_ACTIVE_CLASS);
@@ -144,40 +147,49 @@ class SearchableDropdown {
     }
 
     #highlightPrevOption() {
-        const selectedLi = this.#findSelectedLiOption();
-        if (selectedLi) {
-            if (selectedLi.previousElementSibling) {
-                selectedLi.classList.remove(this.#OPTION_SELECTED_CLASS);
-                selectedLi.previousElementSibling.classList.add(this.#OPTION_SELECTED_CLASS);
-            }
+        const highlightedLi = this.#findHighlightedLiOption();
+        if (highlightedLi && highlightedLi.previousElementSibling) {
+            highlightedLi.classList.remove(this.#OPTION_HIGLIGHTED_CLASS);
+            highlightedLi.previousElementSibling.classList.add(this.#OPTION_HIGLIGHTED_CLASS);
+            this.#scrollToOption(highlightedLi.previousElementSibling);
         } else {
             const liOptions = this.#dropdownOptions.children;
             if (liOptions && liOptions.length > 0) {
-                liOptions[0].classList.add(this.#OPTION_SELECTED_CLASS);
+                liOptions[0].classList.add(this.#OPTION_HIGLIGHTED_CLASS);
+                this.#scrollToOption(liOptions[0]);
             }
         }
     }
 
     #highlightNextOption() {
-        const selectedLi = this.#findSelectedLiOption();
-        if (selectedLi) {
-            if (selectedLi.nextElementSibling) {
-                selectedLi.classList.remove(this.#OPTION_SELECTED_CLASS);
-                selectedLi.nextElementSibling.classList.add(this.#OPTION_SELECTED_CLASS);
+        const highlightedLi = this.#findHighlightedLiOption();
+        if (highlightedLi) {
+            if (highlightedLi.nextElementSibling){
+                highlightedLi.classList.remove(this.#OPTION_HIGLIGHTED_CLASS);
+                highlightedLi.nextElementSibling.classList.add(this.#OPTION_HIGLIGHTED_CLASS);
+                this.#scrollToOption(highlightedLi.nextElementSibling);
             }
-        } else {
+        } else { // highlight first option only in case when no option is currently highlighted
             const liOptions = this.#dropdownOptions.children;
             if (liOptions && liOptions.length > 0) {
-                liOptions[0].classList.add(this.#OPTION_SELECTED_CLASS);
+                liOptions[0].classList.add(this.#OPTION_HIGLIGHTED_CLASS);
+                this.#scrollToOption(liOptions[0]);
             }
+        }
+    }
+
+    #selectHighlightedOption() {
+        const selectedLi = this.#findHighlightedLiOption();
+        if (selectedLi) {
+            this.#updateSelectedValue(Array.from(this.#hiddenSourceSelect.options), selectedLi);
         }
     }
 
     #filterOptionsAccordingToSearchInput() {
         let searchedVal = this.#searchInput.value;
-        const options = Array.from(this.#hiddenSourceSelect.options); //fetch up-to-date options from the hidden select source
+        const options = Array.from(this.#hiddenSourceSelect.options); // fetch up-to-date options from the hidden select source
         let filteredOptions = options.filter((option) => {
-            return this.#getLabel(option).toLowerCase().includes(searchedVal.toLowerCase())
+            return this.#getLabel(option).toLowerCase().includes(searchedVal.toLowerCase());
         });
         this.#populateSelectOptions(filteredOptions, options);
     }
@@ -204,11 +216,11 @@ class SearchableDropdown {
     }
 
     #initDropdownSelectOptions() {
-        const dropdownContent = this.#createHtmlElement("div", "searchable-dropdown-content", '')
+        const dropdownContent = this.#createHtmlElement("div", "searchable-dropdown-content", '');
         this.#dropdownWrapper.appendChild(dropdownContent);
 
-        const dropdownSearch = this.#createHtmlElement("div", "searchable-dropdown-search", '')
-        dropdownContent.appendChild(dropdownSearch)
+        const dropdownSearch = this.#createHtmlElement("div", "searchable-dropdown-search", '');
+        dropdownContent.appendChild(dropdownSearch);
 
         this.#searchInput = this.#createHtmlElement("input", '', '');
         this.#searchInput.type = "text";
@@ -223,69 +235,81 @@ class SearchableDropdown {
         this.#dropdownOptions.innerHTML = "";
         // add items from the collection:
         showOptions.forEach(option => {
-            let isSelected = option.innerText === this.#selectedValueLabel.innerText ? this.#OPTION_SELECTED_CLASS : "";
+            let isSelected = option.value === this.#hiddenSourceSelect.value ? this.#OPTION_HIGLIGHTED_CLASS : "";
             let li = this.#createLiElement(option, isSelected);
             li.addEventListener("click", () => {
                 this.#updateSelectedValue(allOptions, li);
             });
             this.#dropdownOptions.appendChild(li);
-        })
+        });
+
+        // ensure the highlighted option is visible:
+        this.#scrollToHighlightedOption();
     }
 
-    #updateHiddenSelectOptions(newOptions) {
-        this.#hiddenSourceSelect.options.length = 0; //erase existing options
-        for (const option of newOptions) {
-            this.#hiddenSourceSelect.options.add(option);
-        }
+    #findHighlightedLiOption() {
+        return this.#dropdownOptions.querySelector(`.${this.#OPTION_HIGLIGHTED_CLASS}`);
     }
 
-    #updateSelectedValue(allItems, selectedLi) {
+    #createLiElement(option, selectedClass) {
+        const li = this.#createHtmlElement("li", selectedClass, this.#getLabel(option));
+        li.setAttribute(this.#CUSTOM_VALUE_ATTR_NAME, option.value);
+        return li;
+    }
+
+    #getLabel(option) {
+        return option ? option.label || option.innerText : "";
+    }
+
+    #getSelectedOptionText() {
+        return this.#hiddenSourceSelect.selectedOptions.length > 0 ? this.#getLabel(this.#hiddenSourceSelect.selectedOptions[0]) : "";
+    }
+
+    #updateSelectedValue(allOptions, selectedLi) {
         this.#searchInput.value = "";
-        this.#dropdownWrapper.classList.remove(this.#DROPDOWN_ACTIVE_CLASS);
         this.#selectedValueLabel.innerText = selectedLi.innerText;
         this.#hiddenSourceSelect.value = selectedLi.getAttribute(this.#CUSTOM_VALUE_ATTR_NAME);
-        this.#populateSelectOptions(allItems, allItems);
+        this.#dropdownWrapper.classList.remove(this.#DROPDOWN_ACTIVE_CLASS); // close the dropdown
 
-        //fire 'change' event:
+        // populate dropdown with all options to be ready for the next usage:
+        this.#populateSelectOptions(allOptions, allOptions);
+
+        // fire 'change' event:
         let event = new Event('change');
         this.#hiddenSourceSelect.dispatchEvent(event);
     }
 
-    #createLiElement(contentData, isSelected) {
-        let li = this.#createHtmlElement("li", isSelected, this.#getLabel(contentData));
-        li.setAttribute(this.#CUSTOM_VALUE_ATTR_NAME, this.#getValue(contentData));
-        return li;
+    #scrollToHighlightedOption() {
+        let highlightedLiOption = this.#findHighlightedLiOption();
+        if (highlightedLiOption) {
+            this.#scrollToOption(highlightedLiOption);
+        }
     }
 
-    #createOptions(labelValueBeans) {
-        return labelValueBeans ? labelValueBeans.map(bean => this.#createOption(bean)) : [];
+    #scrollToOption(option) {
+        if (option) {
+            option.scrollIntoView({block: "nearest", behavior: "instant"});
+        }
     }
 
-    #createOption(labelValueBean) {
-        const option = document.createElement("option");
-        option.innerText = labelValueBean.label;
-        option.value = labelValueBean.value;
-        return option;
+    #createOptions(items) {
+        return items.map(item => new Option(item.label, item.value));
     }
 
-    #createHtmlElement(tagName, className, content) {
-        let element = document.createElement(tagName);
-        element.className = className;
-        element.innerHTML = content;
+    #updateHiddenSelectOptions(options) {
+        this.#hiddenSourceSelect.innerHTML = "";
+        options.forEach(option => this.#hiddenSourceSelect.add(option));
+    }
+
+    #createHtmlElement(tag, className, innerHtml) {
+        const element = document.createElement(tag);
+        if (className) {
+            element.className = className;
+        }
+        if (innerHtml) {
+            element.innerHTML = innerHtml;
+        }
         return element;
-    }
-
-    #getLabel(option) {
-        return option ? option.innerText : "";
-    }
-
-    #getValue(option) {
-        return option ? option.value : "";
-    }
-
-    #getAttribute(element, attributeName) {
-        let attribute = element.getAttribute(attributeName);
-        return attribute ? attribute : '';
     }
 
     #getSearchPlaceholder() {
@@ -293,14 +317,7 @@ class SearchableDropdown {
         return searchLabel ? searchLabel.innerText : "Search...";
     }
 
-    #getSelectedOptionText() {
-        if (this.#hiddenSourceSelect.selectedIndex >= 0) {
-            return this.#hiddenSourceSelect.options[this.#hiddenSourceSelect.selectedIndex].text;
-        }
-        return '';
-    }
-
-    #findSelectedLiOption() {
-        return this.#dropdownOptions.querySelector('li.selected');
+    #getAttribute(element, attributeName) {
+        return element.getAttribute(attributeName) || "";
     }
 }
